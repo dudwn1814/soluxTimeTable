ddee185e3d96c54552a72156ca6348c7a633ece3
AYJ <dudwn1814@naver.com>
2020-12-22 22:54:20 +0900

    chapter 3, 4(setting datebase URL!)

---
+++ app/src/main/java/com/cookandroid/registeration/ValidateRequest.java
@@ -0,0 +1,24 @@
+package com.cookandroid.registeration;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ValidateRequest extends StringRequest {
+
+    final static private String URL = "http:///UserValidate.php";
+    private Map<String, String> parameters;
+
+    public ValidateRequest(String userID, Response.Listener<String> listener){
+        super(Method.POST, URL, listener, null);
+        parameters = new HashMap<>();
+        parameters.put("userID", userID);
+    }
+
+    @Override
+    public Map<String, String> getParams(){
+        return parameters;
+    }
+}
