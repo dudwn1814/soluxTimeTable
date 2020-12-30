18b9d7bb00fdca5b4ce87061ca880036ff4cef4a
AYJ <dudwn1814@naver.com>
2020-12-30 17:07:37 +0900

    chapter 7 finish!

--- app/src/main/java/com/cookandroid/registeration/MainActivity.java
+++ app/src/main/java/com/cookandroid/registeration/MainActivity.java
@@ -8,13 +8,36 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView noticeListView;
+    private NoticeListAdapter adapter;
+    private List<Notice> noticeList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        noticeListView = (ListView)findViewById(R.id.noticeListView);
+        noticeList = new ArrayList<Notice>();
+        noticeList.add(new Notice("공지사항입니다.", "안영주", "2020-12-30"));
+        noticeList.add(new Notice("공지사항입니다.", "안영주", "2020-12-30"));
+        noticeList.add(new Notice("공지사항입니다.", "안영주", "2020-12-30"));
+        noticeList.add(new Notice("공지사항입니다.", "안영주", "2020-12-30"));
+        noticeList.add(new Notice("공지사항입니다.", "안영주", "2020-12-30"));
+        noticeList.add(new Notice("공지사항입니다.", "안영주", "2020-12-30"));
+        noticeList.add(new Notice("공지사항입니다.", "안영주", "2020-12-30"));
+        noticeList.add(new Notice("공지사항입니다.", "안영주", "2020-12-30"));
+        noticeList.add(new Notice("공지사항입니다.", "안영주", "2020-12-30"));
+        noticeList.add(new Notice("공지사항입니다.", "안영주", "2020-12-30"));
+        adapter = new NoticeListAdapter(getApplicationContext(), noticeList);
+        noticeListView.setAdapter(adapter);
 
         final Button courseButton = (Button)findViewById(R.id.courseButton);
         final Button scheduleButton = (Button)findViewById(R.id.scheduleButton);
